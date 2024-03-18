6f0f55fe448134d35a7c707ae2f309208eed03dd
Shubham Rajput <rajput.shubh1993@gmail.com>
2024-03-18 11:42:26 +0530

    Props

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import {Greet} from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Greet1 from './components/Greet1'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Greet />
-        <Welcome />
-        <Hello />
+        {/* <Greet /> */}
+        {/* <Welcome /> */}
+        {/* <Hello /> */}
+        <Greet1 name="Shubham" age="30" />
+        <Greet1 name="Tushar" age="35" />
+        <Greet1 name="Clark" age="40" />
       </header>
     </div>
   );
---
+++ src/components/Greet1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Greet1=(props)=>{
+  return <h1>Username {props.name} and age {props.age}</h1>
+}
+
+export default Greet1
